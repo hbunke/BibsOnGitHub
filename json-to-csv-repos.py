bc30d3237bf1264142cf29b78e1b2f95b5c56c3c
Axel Klinger <axel.klinger@web.de>
2019-11-24 15:40:58 +0100

    Revert "Add URL for repositories" because CSV gets to big
    This reverts commit fbbdd16c96a1be0287e81e0d7afd9f6f8664bc77.

--- json-to-csv-repos.py
+++ json-to-csv-repos.py
@@ -1,14 +1,14 @@
 import json
 
 
-csv_string = "Organisation,Repository,Stars,Forks,Forked,Last updated, URL\n"
+csv_string = "Organisation,Repository,Stars,Forks,Forked,Last updated\n"
 with open("all-libs.json", 'r') as json_file:
     data = json.load(json_file)
     for org in data["organisations"]:
         for repo in org['repositories']:
             # print(type(repo))
             if type(repo) is dict:
-                csv_string += org['name'] + "," + repo['name'] + "," + str(repo['stargazers_count']) + "," + str(repo['forks_count']) + "," + str(repo['fork']) + "," + repo['updated_at'] + "," + repo['html_url'] + "\n"
+                csv_string += org['name'] + "," + repo['name'] + "," + str(repo['stargazers_count']) + "," + str(repo['forks_count']) + "," + str(repo['fork']) + "," + repo['updated_at'] + "\n"
 
 with open('repositories.csv', 'w', encoding='utf8') as csv_file:
     csv_file.write(csv_string)
