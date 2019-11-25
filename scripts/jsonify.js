56ef6f43b760c4884b8ca1efc6b67b9d60c0301a
Philipp Zumstein <zuphilip@gmail.com>
2019-11-25 11:46:06 +0100

    Add license and move script to a new subfolder

--- scripts/jsonify.js
+++ scripts/jsonify.js
@@ -1,5 +1,12 @@
 #!/usr/bin/env node
 
+/*
+ * Copyright 2018 Philipp Zumstein
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+
 var fs = require("fs")
 
 function jsonifyList() {
